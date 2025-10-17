60ad6de7176367b0831d2d13583ffe694c716a4b
handsome <handsome@lyls.com.tw>
2025-10-17 11:12:07 +0800

    Eagle1017-1

--- CalculatorHomework/Main.java
+++ CalculatorHomework/Main.java
@@ -7,33 +7,15 @@ public class Main {
         System.out.println("請輸入第一個數字: ");
         int userInputNum1 = scanner.nextInt();
 
-        System.out.println("請輸入運算符號: ");
+        System.out.println("請輸入運算符號 (+ - * /): ");
         String userInputOperator = scanner.next();
 
         System.out.println("請輸入第二個數字: ");
         int userInputNum2 = scanner.nextInt();
 
-        double result;
-
-        switch (userInputOperator) {
-            case "+":
-                result = userInputNum1 + userInputNum2;
-                break;
-            case "-":
-                result = userInputNum1 - userInputNum2;
-                break;
-            case "*":
-                result = userInputNum1 * userInputNum2;
-                break;
-            case "/":
-                if (userInputNum2 == 0) {
-                    throw new ArithmeticException("除數不可為 0");
-                }
-                result = (double) userInputNum1 / userInputNum2;
-                break;
-            default:
-                throw new IllegalArgumentException("不支援的運算符號: " + userInputOperator);
-        }
+        // ✅ 完全沒 switch-case 了！
+        Operation operation = OperationFactory.getOperation(userInputOperator);
+        double result = operation.apply(userInputNum1, userInputNum2);
 
         System.out.println(userInputNum1 + " " + userInputOperator + " " + userInputNum2 + " = " + result);
 
